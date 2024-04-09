32492830d58d78799f2aee5ea2beae8f997c2e27
Henry Nguyen <belugas2005@gmail.com>
2024-04-09 07:32:25 -0500

    Incomplete puppeteer testing

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,15 @@
+import { Puppeteer } from "puppeteer";
+
+describe("show/hide event details", () => {
+  test("An event element is collapsed by default", async () => {
+    const browser = await Puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3030/");
+
+    await page.waitForSelector(".event");
+    const eventDetails = await page.$(".event .details");
+    expect(eventDetails).toBeNull();
+    browser.close();
+  });
+});
