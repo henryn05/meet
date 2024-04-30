1b4588b8a279ebde1649e669974d5cd0ff8ae7e1
Henry Nguyen <belugas2005@gmail.com>
2024-04-30 06:28:29 -0500

    Add offline capability & atatus integration

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from "atatus-spa";
+atatus.config("229692177e7647c2b2c4fdb8d1427b4d").install();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
