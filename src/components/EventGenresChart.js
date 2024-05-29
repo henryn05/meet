26e9857eb384d0c2d65688b39a7790940d460e1b
Henry Nguyen <belugas2005@gmail.com>
2024-05-29 08:57:27 -0500

    Add percentage display for pie chart

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -2,6 +2,31 @@ import { PieChart, Pie, ResponsiveContainer } from "recharts";
 import { useState, useEffect } from "react";
 
 const EventGenresChart = ({ events }) => {
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
   const [data, setData] = useState("");
   const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
 
@@ -28,7 +53,7 @@ const EventGenresChart = ({ events }) => {
           dataKey="value"
           fill="#8884d8"
           labelLine={false}
-          label
+          label={renderCustomizedLabel}
           outerRadius={130}
         />
       </PieChart>
