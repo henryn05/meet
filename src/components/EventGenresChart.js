b9f840fac4d21a7962e3fd61ee7953803f2f6dbb
Henry Nguyen <belugas2005@gmail.com>
2024-05-29 08:49:24 -0500

    Create pie chart skeleton

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,9 +1,39 @@
-import { PieChart } from "recharts";
+import { PieChart, Pie, ResponsiveContainer } from "recharts";
+import { useState, useEffect } from "react";
 
 const EventGenresChart = ({ events }) => {
-  <PieChart>
+  const [data, setData] = useState("");
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
 
-  </PieChart>
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) => event.summary.includes(genre));
+      return {
+        name: genre,
+        value: filteredEvents.length,
+      };
+    });
+    return data;
+  }
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          labelLine={false}
+          label
+          outerRadius={130}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default EventGenresChart;
